e17795f1294b567b68df9aa88653fb48f2e092cb
Your Name <minajmiha02@gmail.com>
2025-11-05 23:16:58 +0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+// const jwt = require("jsonwebtoken");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+// const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    //   "https://restaurant-6febb.web.app",
+    //   "https://restaurant-6febb.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb://localhost:27017`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const usersCollection = client.db("ElectroMart").collection("users");
+    const productCollection = client.db("ElectroMart").collection("products");
+    const reviewCollection = client.db("ElectroMart").collection("review");
+    const orderCollection = client.db("ElectroMart").collection("order");
+    const paymentCollection = client.db("ElectroMart").collection("payment");
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("restaurant-server is running");
+});
+app.listen(port, () => {
+  console.log(`restaurant-server is running on port ${port}`);
+});
